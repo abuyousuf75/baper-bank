51f75deaf18943fdf3458a0a4e9550598de8f15d
abuyousuf75 <mdabuyousufsajib@gmail.com>
2023-08-15 23:12:26 +0600

    verfication added

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,22 +3,29 @@ document.getElementById('withdraw-btn').addEventListener('click',function(){
     const newWithdrawString = withdrawField.value;
     const newWithdrawAmound = parseFloat(newWithdrawString);
    
+    // clear value 
+    withdrawField.value = '';
 
     const withdrawElement = document.getElementById('withdrawBlance');
     const previousWithdrawString = withdrawElement.innerText;
     const previousWithraw = parseFloat(previousWithdrawString);
-    const currentBlance = previousWithraw+newWithdrawAmound;
-    withdrawElement.innerText =  currentBlance;
-
+  
     // minus value for withraw from total
    
     const totalBlance = document.getElementById('balance-total');
+    //console.log(totalBlance)
     const previousStringTotalBlance = totalBlance.innerText;
     const previouBlance = parseFloat(previousStringTotalBlance);
+   
+    if(newWithdrawAmound > previouBlance){
+        alert ('You dont have enough money for withdraw');
+        return 
+    }
+    const currentBlance = previousWithraw+newWithdrawAmound;
+    withdrawElement.innerText =  currentBlance;
+
     const currentBlanceNew  = previouBlance - currentBlance ;
     totalBlance.innerText = currentBlanceNew;
-  
 
-    // clear value 
-    withdrawField.value = '';
+
 })
